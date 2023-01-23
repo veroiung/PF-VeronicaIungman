39299cc239f28a6cc98866bb11f80c89e2fa08d8
veroiung <dg@veronicaiungman.com.ar>
2023-01-23 17:05:08 -0300

    php

--- sendemail.php
+++ sendemail.php
@@ -14,27 +14,24 @@ if ((strlen($name) > 0) and (strlen($email) > 0) and (strlen($asunto) > 0)) {
 
     $contenido = "$name ha enviado un mensaje desde la web www.playlisteros.com.ar\n"
     . "\n"
-. "Nombre y Apellido: $name\n"
-. "Email: $email\n"
-. "Asunto: $asunto\n"
-. "Mensaje: $message\n"
-. "\n";
+    . "Nombre: $name\n"
+    . "Email: $email\n"
+    . "Asunto: $asunto\n"
+    . "Mensaje: $message\n"
+     "\n";
  
-
 if (@mail($email_to, $subject, $contenido, $header)) {
  
 echo'<script>alert("Gracias, su mensaje se envio correctamente.") ; </script>';
 
-echo'<SCRIPT language="javascript">
-	location.href = "index.html";
+echo'<SCRIPT language="javascript">location.href = "index.html";
 	</SCRIPT>';
 }
 
 else{
 echo'<script>alert("Error: Su información no pudo ser enviada, intente más tarde.") ; </script>';
 
-echo'<SCRIPT language="javascript">
-	location.href = "index.html";
+echo'<SCRIPT language="javascript">location.href = "index.html";
 	</SCRIPT>';
 	
 }
